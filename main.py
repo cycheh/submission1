b6d70dc48c41fa06b588054194c67d95f3e75223
cycheh <cycheh@hotmail.com>
2025-11-03 11:12:21 +0800

    add changes to main.py

--- main.py
+++ main.py
@@ -20,6 +20,16 @@ st.set_page_config(
 
 st.title("Streamlit App")
 
+with st.expander(":mag_right: Disclaimer"):
+    st.write("IMPORTANT NOTICE: This web application is a prototype developed for educational purposes only.")
+
+    st.write("The information provided here is NOT intended for real-world usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.")
+
+    st.write("Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.")
+
+    st.write("Always consult with qualified professionals for accurate and personalized advice.")
+    
+
 form = st.form(key="form")
 form.subheader("Prompt")
 

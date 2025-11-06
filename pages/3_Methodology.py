284d0556bcc21b223a96e234064cb279bd2728d5
cycheh <cycheh@hotmail.com>
2025-11-06 17:36:04 +0800

    changed methodology page

--- pages/3_Methodology.py
+++ pages/3_Methodology.py
@@ -14,9 +14,9 @@ st.title(":bar_chart: Methodology")
 
 st.write(":speech_balloon: The flowchart below shows the methodology and data sources that were used to develop the School Navigator Streamlit App. ")
 
-st.image(image, caption='Methodology Flow Chart', use_column_width=True)
+st.image(image, caption='', use_container_width=True)
 
-with st.expander("How to use this App"):
+with st.expander("How to use this App :hibiscus:"):
     st.write("1. Enter your prompt in the chat box on the main page.")
     st.write("2. Click the 'Submit' button.")
     st.write("3. The app will generate an answer based on your query. Do refer to the sample questions on the main page for the types of queries it can be used for :sunny:")

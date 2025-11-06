d87fe992e8fe3a9965e1156e683e208a19c4c11b
cycheh <cycheh@hotmail.com>
2025-11-06 17:32:52 +0800

    methodology flowchart

--- pages/3_Methodology.py
+++ pages/3_Methodology.py
@@ -1,4 +1,7 @@
 import streamlit as st
+from PIL import Image
+
+image = Image.open('pages/streamlit_methodology_flowchart.jpg')
 
 # region <--------- Streamlit App Configuration --------->
 st.set_page_config(
@@ -7,11 +10,13 @@ st.set_page_config(
 )
 # endregion <--------- Streamlit App Configuration --------->
 
-st.title("About this App")
+st.title(":bar_chart: Methodology")
 
-st.write("This is a Streamlit App that demonstrates how to use the OpenAI API to generate text completions.")
+st.write(":speech_balloon: The flowchart below shows the methodology and data sources that were used to develop the School Navigator Streamlit App. ")
+
+st.image(image, caption='Methodology Flow Chart', use_column_width=True)
 
 with st.expander("How to use this App"):
-    st.write("1. Enter your prompt in the text area.")
+    st.write("1. Enter your prompt in the chat box on the main page.")
     st.write("2. Click the 'Submit' button.")
-    st.write("3. The app will generate a text completion based on your prompt.")
+    st.write("3. The app will generate an answer based on your query. Do refer to the sample questions on the main page for the types of queries it can be used for :sunny:")
